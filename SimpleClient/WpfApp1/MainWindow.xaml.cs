50d1aa6fbcf4c664a60af160996e56e95a015930
jaimecastre <jaime.castresana@hbkworld.com>
2025-01-16 09:41:53 +0100

    commit

--- SimpleClient/WpfApp1/MainWindow.xaml.cs
+++ SimpleClient/WpfApp1/MainWindow.xaml.cs
@@ -17,12 +17,17 @@ using SciChart.Charting.Visuals.RenderableSeries;
 using SciChart.Data.Model;
 using System.Numerics;
 using MathNet.Numerics.IntegralTransforms;
+using SciChart.Charting.Model.DataSeries.Heatmap2DArrayDataSeries;
 
 namespace WpfApp1
 {
     public partial class MainWindow : Window
     {
         private bool _run = true;
+        private const int HeatmapWidth = 1024;
+        private const int HeatmapHeight = 100;
+        private double[,] _heatmapData = new double[HeatmapHeight, HeatmapWidth];
+        private int _currentRow = 0;
 
         public MainWindow()
         {
@@ -38,66 +43,42 @@ namespace WpfApp1
 
         private void InitializeSciChart()
         {
-            sciChartSurface.XAxes.Add(new NumericAxis());
-            sciChartSurface.YAxes.Add(new NumericAxis { AxisAlignment = AxisAlignment.Left });
+            var xAxis = new NumericAxis
+            {
+                AxisTitle = "Frequency (Hz)",
+                AxisAlignment = AxisAlignment.Bottom
+            };
+
+            var yAxis = new NumericAxis
+            {
+                AxisTitle = "Amplitude (dB SPL)",
+                AxisAlignment = AxisAlignment.Left
+            };
+
+            sciChartSurface.XAxes.Add(xAxis);
+            sciChartSurface.YAxes.Add(yAxis);
+
+            var heatmapXAxis = new NumericAxis
+            {
+                AxisTitle = "Time",
+                AxisAlignment = AxisAlignment.Bottom
+            };
+
+            var heatmapYAxis = new NumericAxis
+            {
+                AxisTitle = "Frequency (Hz)",
+                AxisAlignment = AxisAlignment.Left
+            };
+
+            heatmapSurface.XAxes.Add(heatmapXAxis);
+            heatmapSurface.YAxes.Add(heatmapYAxis);
         }
 
         private void btStart_Click(object sender, RoutedEventArgs e)
         {
             StartListenerAsync();
-
-            //Sending the data to Python
-            //using (var requestSocket = new RequestSocket("tcp://127.0.0.1:5555"))
-            //{
-                //var newData = new PythonInputData();
-                //newData.overall = 60;
-                //newData.spec = new double[1024];
-                //var random = new Random();
-                //for (int i = 0; i < newData.spec.Length; i++)
-                //{
-                //    newData.spec[i] = random.NextDouble();
-                //}
-
-                //var serializedData = JsonConvert.SerializeObject(newData);
-                //requestSocket.SendFrame(serializedData);
-
-                //Receiving the data from Python
-                //var message = requestSocket.ReceiveFrameString();
-                //lbSPL.Content = "Error: " + (Math.Round(Convert.ToDouble(message) - 100)).ToString() + "dB";
-
-                //// Calculate FFT
-                //Complex[] fftResult = new Complex[newData.spec.Length];
-                //for (int i = 0; i < newData.spec.Length; i++)
-                //{
-                //    fftResult[i] = new Complex(newData.spec[i], 0);
-                //}
-                //Fourier.Forward(fftResult, FourierOptions.Matlab);
-
-                //// Calculate average FFT in dB SPL
-                //double[] avgFftDbSpl = new double[fftResult.Length / 2];
-                //for (int i = 0; i < avgFftDbSpl.Length; i++)
-                //{
-                //    avgFftDbSpl[i] = 20 * Math.Log10(fftResult[i].Magnitude);
-                //}
-
-                //// Plotting the average FFT in dB SPL
-                //var dataSeries = new XyDataSeries<double, double>();
-                //for (int i = 0; i < avgFftDbSpl.Length; i++)
-                //{
-                //    dataSeries.Append(i, avgFftDbSpl[i]);
-                //}
-
-                //var lineSeries = new FastLineRenderableSeries
-                //{
-                //    DataSeries = dataSeries
-                //};
-
-                //sciChartSurface.RenderableSeries.Clear();
-                //sciChartSurface.RenderableSeries.Add(lineSeries);
-            //}
         }
-    
-    
+
         private async void StartListenerAsync()
         {
             await Task.Run(Listener);
@@ -105,25 +86,23 @@ namespace WpfApp1
 
         private void Listener()
         {
-            using (var socket = new RequestSocket())
+            using (var socket = new RequestSocket("tcp://127.0.0.1:5555"))
             {
-                socket.Bind("tcp://*:5555");
-
                 socket.SendFrame("START");
-                while (true)
+                var resp = socket.ReceiveFrameString();
+                if (resp != "OK")
                 {
-                    var resp = socket.ReceiveFrameString();
-                    if (resp == "OK")
-                        break;
+                    Console.WriteLine($"Unexpected response: {resp}");
+                    return;
                 }
 
                 _run = true;
                 while (_run)
                 {
-                    string str = socket.ReceiveFrameString();
+                    string str = resp; //socket.ReceiveFrameString();
                     switch (str)
                     {
-                        case "STARTSPC":
+                        case "OK":
                             CommandStartSpc(socket);
                             break;
 
@@ -131,22 +110,16 @@ namespace WpfApp1
                             CommandShowValue(socket);
                             break;
 
-                        //case "PLOTDATA":
-                        //    CommandPlotData(socket);
-                        //    break;
-
                         default:
                             Console.WriteLine($"Unexpected message: {str}");
                             break;
                     }
 
-
                     Thread.Sleep(1000);
-
                 }
             }
         }
-    
+
         private void CommandStartSpc(RequestSocket socket)
         {
             var newData = new PythonInputData();
@@ -159,7 +132,7 @@ namespace WpfApp1
             }
 
             var serializedData = JsonConvert.SerializeObject(newData);
-            socket.SendFrame(serializedData);
+            //socket.SendFrame(serializedData);
 
             // Calculate FFT
             Complex[] fftResult = new Complex[newData.spec.Length];
@@ -183,56 +156,39 @@ namespace WpfApp1
                 dataSeries.Append(i, avgFftDbSpl[i]);
             }
 
-            var lineSeries = new FastLineRenderableSeries
+            Dispatcher.Invoke(() =>
             {
-                DataSeries = dataSeries
-            };
+                var lineSeries = new FastLineRenderableSeries
+                {
+                    DataSeries = dataSeries
+                };
 
-            sciChartSurface.RenderableSeries.Clear();
-            sciChartSurface.RenderableSeries.Add(lineSeries);
+                sciChartSurface.RenderableSeries.Clear();
+                sciChartSurface.RenderableSeries.Add(lineSeries);
+
+                // Update heatmap data
+                for (int i = 0; i < avgFftDbSpl.Length; i++)
+                {
+                    _heatmapData[_currentRow, i] = avgFftDbSpl[i];
+                }
+                _currentRow = (_currentRow + 1) % HeatmapHeight;
+
+                var heatmapDataSeries = new UniformHeatmapDataSeries<double, double, double>(_heatmapData, 0, 1, 0, 1);
+                heatmapSeries.DataSeries = heatmapDataSeries;
+            });
         }
 
         private void CommandShowValue(RequestSocket socket)
         {
             var message = socket.ReceiveFrameString();
-            lbSPL.Content = "Error: " + (Math.Round(Convert.ToDouble(message) - 100)).ToString() + "dB";
+            if (double.TryParse(message, out double value))
+            {
+                lbSPL.Content = $"Error: {Math.Round(value - 100)} dB";
+            }
+            else
+            {
+                lbSPL.Content = "Error: Invalid data";
+            }
         }
-
-        //private void CommandPlotData(RequestSocket socket)
-        //{
-        //    int length = 1024;
-
-
-        //    // Calculate FFT
-        //    Complex[] fftResult = new Complex[length];
-        //    for (int i = 0; i < length; i++)
-        //    {
-        //        fftResult[i] = new Complex(newData.spec[i], 0);
-        //    }
-        //    Fourier.Forward(fftResult, FourierOptions.Matlab);
-
-        //    // Calculate average FFT in dB SPL
-        //    double[] avgFftDbSpl = new double[fftResult.Length / 2];
-        //    for (int i = 0; i < avgFftDbSpl.Length; i++)
-        //    {
-        //        avgFftDbSpl[i] = 20 * Math.Log10(fftResult[i].Magnitude);
-        //    }
-
-        //    // Plotting the average FFT in dB SPL
-        //    var dataSeries = new XyDataSeries<double, double>();
-        //    for (int i = 0; i < avgFftDbSpl.Length; i++)
-        //    {
-        //        dataSeries.Append(i, avgFftDbSpl[i]);
-        //    }
-
-        //    var lineSeries = new FastLineRenderableSeries
-        //    {
-        //        DataSeries = dataSeries
-        //    };
-
-        //    sciChartSurface.RenderableSeries.Clear();
-        //    sciChartSurface.RenderableSeries.Add(lineSeries);
-
-        //}
     }
 }
